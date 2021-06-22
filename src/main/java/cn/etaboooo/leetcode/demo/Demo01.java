3646b5e32c2ec7ec477f78f9199f9c43bd76665e
eatboooo <571241210@qq.com>
2021-06-22 10:38:34 +0800

    53. 最大子序和 - 动态规划 需要复习

--- src/main/java/cn/etaboooo/leetcode/demo/Demo01.java
+++ src/main/java/cn/etaboooo/leetcode/demo/Demo01.java
@@ -730,4 +730,24 @@ public class Demo01 {
         }
         return result;
     }
+
+
+    /**
+     * https://leetcode-cn.com/problems/maximum-subarray/
+     * 动态规划，需要复习
+     * @Description: 53. 最大子序和
+     * @Param: [nums]
+     * @return: int
+     * @Author: weiZhiLin
+     * @Date: 2021/6/22 10:33
+     */
+    public int maxSubArray(int[] nums) {
+        int pre = 0;
+        int max = nums[0];
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            max = Math.max(max, pre);
+        }
+        return max;
+    }
 }
