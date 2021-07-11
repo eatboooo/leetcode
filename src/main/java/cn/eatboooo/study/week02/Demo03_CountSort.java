b0dd4a06dc881f79aeb9efa8d63e66cc6e163e32
eatboooo <571241210@qq.com>
2021-07-12 01:20:26 +0800

    算法学习 week 02，桶排序

---
+++ src/main/java/cn/eatboooo/study/week02/Demo03_CountSort.java
@@ -0,0 +1,120 @@
+package cn.eatboooo.study.week02;
+
+import java.util.Arrays;
+
+/**
+ * @author weiZhiLin
+ * @version 1.0
+ * @date 2021/7/12 01:10
+ * 桶排序
+ */
+public class Demo03_CountSort {
+    // 桶排序对样本有限制
+    // 0~200
+    public static void countSort(int[] arr) {
+        if (arr.length < 2) {
+            return;
+        }
+        // 求出数组最大值
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(arr[i], max);
+        }
+        int[] help = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            help[arr[i]]++;
+        }
+        int index = 0;
+        // 这里是桶的长度！！！
+        for (int i = 0; i < help.length; i++) {
+            while (help[i]-- > 0) {
+                arr[index++] = i;
+            }
+        }
+    }
+
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 150;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            countSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        countSort(arr);
+        printArray(arr);
+
+    }
+
+}
