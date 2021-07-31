f231b9c4297b16065741d5eb6f8776d1ccaf1b96
eatboooo <571241210@qq.com>
2021-07-31 23:45:34 +0800

    week 04，贴纸题目

--- src/main/java/cn/eatboooo/study/week04/Demo08_Try_Sticker.java
+++ src/main/java/cn/eatboooo/study/week04/Demo08_Try_Sticker.java
@@ -1,5 +1,7 @@
 package cn.eatboooo.study.week04;
 
+import java.util.HashMap;
+
 /**
  * @author weiZhiLin
  * @version 1.0
@@ -61,4 +63,108 @@ public class Demo08_Try_Sticker {
     }
 
 
+    public static int minSticker2(String target, String[] stickers) {
+        char[] chars = target.toCharArray();
+        // 贴纸词频表
+        int[][] strArr = new int[stickers.length][27];
+        for (int i = 0; i < stickers.length; i++) {
+            char[] what = stickers[i].toCharArray();
+            for (char c : what) {
+                strArr[i][c - 'a']++;
+            }
+        }
+        int ans = process2(strArr, target);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    private static int process2(int[][] strArr, String strrr) {
+        if (strrr.length() == 0) {
+            return 0;
+        }
+        // 统计 str 词频
+        char[] chars = strrr.toCharArray();
+        int[] restArr = new int[26];
+        for (char aChar : chars) {
+            restArr[aChar - 'a']++;
+        }
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < strArr.length; i++) {
+            // 词频对照，如果合适就相减
+            if (strArr[i][chars[0] - 'a'] > 0) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < 26; j++) {
+                    if (restArr[j] > 0) {
+                        // 第一次相减写反了
+                        for (int k = 0; k < restArr[j] - strArr[i][j]; k++) {
+                            sb.append((char) ('a' + j));
+                        }
+                    }
+                }
+                min = Math.min(min, process2(strArr, sb.toString()));
+            }
+        }
+        return min + (min == Integer.MAX_VALUE ? 0 : 1);
+    }
+
+    public static int minSticker3(String target, String[] arr) {
+        if (target.length() == 0 || arr.length == 0) {
+            return -1;
+        }
+        int[][] stickers = new int[arr.length][26];
+        for (int i = 0; i < arr.length; i++) {
+            char[] chars = arr[i].toCharArray();
+            for (char aChar : chars) {
+                stickers[i][aChar - 'a']++;
+            }
+        }
+
+        int ans = process3(target, stickers, new HashMap<String, Integer>());
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    private static int process3(String target, int[][] stickers, HashMap<String, Integer> dp) {
+        if (dp.containsKey(target)) {
+            return dp.get(target);
+        }
+        if (target.length() == 0) {
+            return 0;
+        }
+        char[] chars = target.toCharArray();
+        int[] targets = new int[26];
+        for (char aChar : chars) {
+            targets[aChar - 'a']++;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < stickers.length; i++) {
+            if (stickers[i][chars[0] - 'a'] > 0) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < 26; j++) {
+                    if (targets[j] > 0) {
+                        for (int k = 0; k < targets[j] - stickers[i][j]; k++) {
+                            sb.append((char) (j + 'a'));
+                        }
+                    }
+                }
+                min = Math.min(min, process3(sb.toString(), stickers, dp));
+            }
+        }
+        int ans = min + (min == Integer.MAX_VALUE ? 0 : 1);
+        dp.put(target, ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                ["notice","possible"]
+        "basicbasic"*/
+        int thehat = minSticker("thehat", new String[]{"with", "example", "science"});
+        System.out.println("thehat = " + thehat);
+        thehat = minSticker2("thehat", new String[]{"with", "example", "science"});
+        System.out.println("thehat = " + thehat);
+        thehat = minSticker3("thehat", new String[]{"with", "example", "science"});
+        System.out.println("thehat = " + thehat);
+
+    }
 }
