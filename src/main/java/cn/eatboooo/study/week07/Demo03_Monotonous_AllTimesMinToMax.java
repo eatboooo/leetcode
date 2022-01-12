bb0c871f064f43ebc077101abd67f02d22115fe8
禁止种太阳 <571241210@qq.com>
2022-01-12 21:01:38 +0800

    单调栈-子数组Max（(sub累加和 )* (sub中的最小值)）

--- src/main/java/cn/eatboooo/study/week07/Demo03_Monotonous_AllTimesMinToMax.java
+++ src/main/java/cn/eatboooo/study/week07/Demo03_Monotonous_AllTimesMinToMax.java
@@ -1,5 +1,7 @@
 package cn.eatboooo.study.week07;
 
+import java.util.Stack;
+
 /**
  * 单调栈
  *
@@ -13,6 +15,28 @@ package cn.eatboooo.study.week07;
  */
 public class Demo03_Monotonous_AllTimesMinToMax {
     public static int max1(int[] arr) {
-        return -1;
+        // 前缀和
+        int[] sum = new int[arr.length];
+        sum[0] = arr[0];
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = arr[i] + sum[i - 1];
+        }
+        int max = Integer.MIN_VALUE;
+        // 单调栈，不考虑数组重复的，因为后门的数字会纠错
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            int i1 = arr[i];
+            while (!stack.isEmpty() && arr[stack.peek()] > i1) {
+                Integer pop = stack.pop();
+                max = Math.max((stack.isEmpty() ? sum[i - 1] :sum[i - 1] - sum[stack.peek()]) * arr[pop], max);
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            Integer pop = stack.pop();
+            // ⚠️ 注意 stack 空时的判断
+            max = Math.max((stack.isEmpty() ? sum[arr.length - 1] :sum[arr.length - 1] - sum[stack.peek()]) * arr[pop], max);
+        }
+        return max;
     }
 }
