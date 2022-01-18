ca70d4b35ba6335030a28cbf12f4795bae8b3f00
禁止种太阳 <571241210@qq.com>
2022-01-18 21:24:10 +0800

    蓄水池题目

---
+++ src/main/java/cn/eatboooo/study/week07/Demo08_ReservoirSampling_Base.java
@@ -0,0 +1,17 @@
+package cn.eatboooo.study.week07;
+
+/**
+ * 蓄水池算法
+ *
+ * @author weiZhiLin
+ * @version 1.0
+ * @date 2022/1/17 20:08
+ *
+ * 一个管道一直出球，一个池子里面只能装固定的球 为 size
+ * 怎么样设计，让管子出的球都等概率进池子
+ * 出的球编号从1递增为 i
+ * 每个球进管子的概率为 size / i  及为答案
+ *
+ */
+public class Demo08_ReservoirSampling_Base {
+}
