722122ecdc90a6040da6dae6083ccb1088371e61
禁止种太阳 <571241210@qq.com>
2022-02-09 20:25:18 +0800

    AVL 树实现

--- src/main/java/cn/eatboooo/study/week09/Demo01_Table_AVL.java
+++ src/main/java/cn/eatboooo/study/week09/Demo01_Table_AVL.java
@@ -7,6 +7,294 @@ package cn.eatboooo.study.week09;
  * @version 1.0
  * @date 2022/2/8 20:03
  *
+ * 左右子树的高度差不超过 1，叫平衡
+ * copy for study
  */
 public class Demo01_Table_AVL {
+    // AVLNode, k ,vale
+    // k 必须实现 comparable
+    // h 代表高度
+    public static class AVLNode<K extends Comparable<K>, V> {
+        public K k;
+        public V v;
+        public AVLNode<K, V> l;
+        public AVLNode<K, V> r;
+        public int h;
+
+        public AVLNode(K key, V value) {
+            k = key;
+            v = value;
+            h = 1;
+        }
+    }
+
+    // AVLTree，root，size
+    // 必须包含根节点
+    // 否则无法查找
+    public static class AVLTreeMap<K extends Comparable<K>, V> {
+        private AVLNode<K, V> root;
+        private int size;
+
+        public AVLTreeMap() {
+            root = null;
+            size = 0;
+        }
+
+        // 左旋
+        private AVLNode<K, V> rightRotate(AVLNode<K, V> cur) {
+            AVLNode<K, V> left = cur.l;
+            cur.l = left.r;
+            left.r = cur;
+            cur.h = Math.max((cur.l != null ? cur.l.h : 0), (cur.r != null ? cur.r.h : 0)) + 1;
+            left.h = Math.max((left.l != null ? left.l.h : 0), (left.r != null ? left.r.h : 0)) + 1;
+            return left;
+        }
+
+        // 右旋
+        private AVLNode<K, V> leftRotate(AVLNode<K, V> cur) {
+            AVLNode<K, V> right = cur.r;
+            cur.r = right.l;
+            right.l = cur;
+            cur.h = Math.max((cur.l != null ? cur.l.h : 0), (cur.r != null ? cur.r.h : 0)) + 1;
+            right.h = Math.max((right.l != null ? right.l.h : 0), (right.r != null ? right.r.h : 0)) + 1;
+            return right;
+        }
+
+        // 平衡调整
+        // 不平衡的情况：左左、左右；右右、右左
+        private AVLNode<K, V> maintain(AVLNode<K, V> cur) {
+            if (cur == null) {
+                return null;
+            }
+            int leftHeight = cur.l != null ? cur.l.h : 0;
+            int rightHeight = cur.r != null ? cur.r.h : 0;
+            if (Math.abs(leftHeight - rightHeight) > 1) {
+                if (leftHeight > rightHeight) {
+                    // 此时有两种不平衡的情况：左左、左右
+                    int leftLeftHeight = cur.l != null && cur.l.l != null ? cur.l.l.h : 0;
+                    int leftRightHeight = cur.l != null && cur.l.r != null ? cur.l.r.h : 0;
+                    if (leftLeftHeight >= leftRightHeight) {
+                        // 此时是左左违规，同时包含了特殊情况的左右
+                        cur = rightRotate(cur);
+                    } else {
+                        // 此时左右违规，两次旋转搞定
+                        cur.l = leftRotate(cur.l);
+                        cur = rightRotate(cur);
+                    }
+                } else {
+                    // copy 上面的，左右反转
+                    int rightLeftHeight = cur.r != null && cur.r.l != null ? cur.r.l.h : 0;
+                    int rightRightHeight = cur.r != null && cur.r.r != null ? cur.r.r.h : 0;
+                    if (rightRightHeight >= rightLeftHeight) {
+                        cur = leftRotate(cur);
+                    } else {
+                        cur.r = rightRotate(cur.r);
+                        cur = leftRotate(cur);
+                    }
+                }
+            }
+            return cur;
+        }
+
+        // 找到最接近的值，比他小往左，比他大往右
+        private AVLNode<K, V> findLastIndex(K key) {
+            AVLNode<K, V> pre = root;
+            AVLNode<K, V> cur = root;
+            while (cur != null) {
+                pre = cur;
+                if (key.compareTo(cur.k) == 0) {
+                    break;
+                } else if (key.compareTo(cur.k) < 0) {
+                    cur = cur.l;
+                } else {
+                    cur = cur.r;
+                }
+            }
+            return pre;
+        }
+
+        // 找到最接近它，比它大的
+        private AVLNode<K, V> findLastNoSmallIndex(K key) {
+            AVLNode<K, V> ans = null;
+            AVLNode<K, V> cur = root;
+            while (cur != null) {
+                if (key.compareTo(cur.k) == 0) {
+                    ans = cur;
+                    break;
+                } else if (key.compareTo(cur.k) < 0) {
+                    ans = cur;
+                    cur = cur.l;
+                } else {
+                    cur = cur.r;
+                }
+            }
+            return ans;
+        }
+
+        // 找到最接近它，比它小的
+        private AVLNode<K, V> findLastNoBigIndex(K key) {
+            AVLNode<K, V> ans = null;
+            AVLNode<K, V> cur = root;
+            while (cur != null) {
+                if (key.compareTo(cur.k) == 0) {
+                    ans = cur;
+                    break;
+                } else if (key.compareTo(cur.k) < 0) {
+                    cur = cur.l;
+                } else {
+                    ans = cur;
+                    cur = cur.r;
+                }
+            }
+            return ans;
+        }
+
+        // 添加节点
+        private AVLNode<K, V> add(AVLNode<K, V> cur, K key, V value) {
+            if (cur == null) {
+                return new AVLNode<K, V>(key, value);
+            } else {
+                if (key.compareTo(cur.k) < 0) {
+                    // 左侧可能换头，需要接住
+                    cur.l = add(cur.l, key, value);
+                } else {
+                    // 右侧可能换头，需要接住
+                    cur.r = add(cur.r, key, value);
+                }
+                // 楼层 +1
+                cur.h = Math.max(cur.l != null ? cur.l.h : 0, cur.r != null ? cur.r.h : 0) + 1;
+                // 返回调整后的结果
+                return maintain(cur);
+            }
+        }
+
+        // 在cur这棵树上，删掉key所代表的节点
+        // 返回cur这棵树的新头部
+        private AVLNode<K, V> delete(AVLNode<K, V> cur, K key) {
+            if (key.compareTo(cur.k) > 0) {
+                cur.r = delete(cur.r, key);
+            } else if (key.compareTo(cur.k) < 0) {
+                cur.l = delete(cur.l, key);
+            } else {
+                if (cur.l == null && cur.r == null) {
+                    // 此时 key 没有孩子，直接滚蛋
+                    cur = null;
+                } else if (cur.l == null && cur.r != null) {
+                    // 此时 key 有一个孩子，让他直接继承
+                    cur = cur.r;
+                } else if (cur.l != null && cur.r == null) {
+                    // 此时 key 有一个孩子，让他直接继承
+                    cur = cur.l;
+                } else {
+                    // 此时 key 有两个孩子
+                    // 用它最接近它的节点来替代它（右树最左/左树最右）
+                    AVLNode<K, V> des = cur.r;
+                    while (des.l != null) {
+                        // 替代了
+                        des = des.l;
+                    }
+                    // 删除了右树上的最左
+                    cur.r = delete(cur.r, des.k);
+                    // 替代品设置左右孩子
+                    des.l = cur.l;
+                    des.r = cur.r;
+                    // 当前节点来到删除后的替代节点
+                    cur = des;
+                }
+            }
+            if (cur != null) {
+                cur.h = Math.max(cur.l != null ? cur.l.h : 0, cur.r != null ? cur.r.h : 0) + 1;
+            }
+            return maintain(cur);
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public boolean containsKey(K key) {
+            if (key == null) {
+                return false;
+            }
+            AVLNode<K, V> lastNode = findLastIndex(key);
+            return lastNode != null && key.compareTo(lastNode.k) == 0 ? true : false;
+        }
+
+        public void put(K key, V value) {
+            if (key == null) {
+                return;
+            }
+            AVLNode<K, V> lastNode = findLastIndex(key);
+            if (lastNode != null && key.compareTo(lastNode.k) == 0) {
+                lastNode.v = value;
+            } else {
+                size++;
+                root = add(root, key, value);
+            }
+        }
+
+        public void remove(K key) {
+            if (key == null) {
+                return;
+            }
+            if (containsKey(key)) {
+                size--;
+                root = delete(root, key);
+            }
+        }
+
+        public V get(K key) {
+            if (key == null) {
+                return null;
+            }
+            AVLNode<K, V> lastNode = findLastIndex(key);
+            if (lastNode != null && key.compareTo(lastNode.k) == 0) {
+                return lastNode.v;
+            }
+            return null;
+        }
+
+        // 第一个 key，最左
+        public K firstKey() {
+            if (root == null) {
+                return null;
+            }
+            AVLNode<K, V> cur = root;
+            while (cur.l != null) {
+                cur = cur.l;
+            }
+            return cur.k;
+        }
+
+        // 最后一个 key，最右
+        public K lastKey() {
+            if (root == null) {
+                return null;
+            }
+            AVLNode<K, V> cur = root;
+            while (cur.r != null) {
+                cur = cur.r;
+            }
+            return cur.k;
+        }
+
+        // 最接近 key 的且比它小
+        public K floorKey(K key) {
+            if (key == null) {
+                return null;
+            }
+            AVLNode<K, V> lastNoBigNode = findLastNoBigIndex(key);
+            return lastNoBigNode == null ? null : lastNoBigNode.k;
+        }
+
+        // 最接近 key 的且比它大
+        public K ceilingKey(K key) {
+            if (key == null) {
+                return null;
+            }
+            AVLNode<K, V> lastNoSmallNode = findLastNoSmallIndex(key);
+            return lastNoSmallNode == null ? null : lastNoSmallNode.k;
+        }
+
+    }
 }
