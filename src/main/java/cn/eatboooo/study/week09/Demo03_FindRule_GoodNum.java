db0fce8ef681a9088e0f1ef16056062794e0130e
禁止种太阳 <571241210@qq.com>
2022-02-11 19:19:15 +0800

    连续数字找规律

--- src/main/java/cn/eatboooo/study/week09/Demo03_FindRule_GoodNum.java
+++ src/main/java/cn/eatboooo/study/week09/Demo03_FindRule_GoodNum.java
@@ -11,4 +11,33 @@ package cn.eatboooo.study.week09;
  * 给定一个参数N，返回是不是可以表示成若干连续正数和的数
  */
 public class Demo03_FindRule_GoodNum {
+    public static boolean goodNum(int num) {
+        int temp = 0;
+        for (int i = 1; i < num; i++) {
+            temp = 0;
+            for (int j = i; j < num; j++) {
+                temp += j;
+                if (temp == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean myRules(int num) {
+        return (num & (-num)) != num;
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            boolean b = goodNum(i);
+            boolean m = myRules(i);
+            if (m != b) {
+                System.out.println(i + "!!!!!!");
+            }
+//            if (!b) {
+//                System.out.println(i + " = " + b);
+//            }
+        }
+    }
 }
