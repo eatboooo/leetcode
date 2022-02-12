ad4cc8c078dbcc5c23eec771ed1ee8b2837e6c21
禁止种太阳 <571241210@qq.com>
2022-02-12 21:52:21 +0800

    卡特兰数

--- src/main/java/cn/eatboooo/study/week09/Demo04_Cattelen_OneZero.java
+++ src/main/java/cn/eatboooo/study/week09/Demo04_Cattelen_OneZero.java
@@ -11,4 +11,26 @@ package cn.eatboooo.study.week09;
  * 返回有多少个序列满足任何前缀串，1的数量都不少于0的数量
  */
 public class Demo04_Cattelen_OneZero {
+    // 经典卡特兰数问题，参考括号问题
+    // C（2n,n） = (2n!)/(n!*(2n - n)!) = (n~2n)!/n!
+    // 卡特兰数 ： C(2n,n) / n + 1
+    public static long ways2(int N) {
+        if (N < 0) {
+            return 0;
+        }
+        if (N < 2) {
+            return 1;
+        }
+        long a = 1;
+        long b = 1;
+        long limit = N << 1;
+        for (long i = 1; i <= limit; i++) {
+            if (i <= N) {
+                a *= i;
+            } else {
+                b *= i;
+            }
+        }
+        return (b / a) / (N + 1);
+    }
 }
--- src/main/java/cn/eatboooo/study/week09/Demo04_Cattelen_Tree.java
+++ src/main/java/cn/eatboooo/study/week09/Demo04_Cattelen_Tree.java
@@ -18,4 +18,28 @@ public class Demo04_Cattelen_Tree {
     //	k(n) = c(2n, n) / (n + 1)
     //	或者
     //	k(n) = c(2n, n) - c(2n, n-1)
+
+    // 同样的卡特兰数问题，直接套用公式
+    // 经典卡特兰数问题，参考括号问题
+    // C（2n,n） = (2n!)/(n!*(2n - n)!) = (n~2n)!/n!
+    // 卡特兰数 ： C(2n,n) / n + 1
+    public static long ways2(int N) {
+        if (N < 0) {
+            return 0;
+        }
+        if (N < 2) {
+            return 1;
+        }
+        long a = 1;
+        long b = 1;
+        long limit = N << 1;
+        for (long i = 1; i <= limit; i++) {
+            if (i <= N) {
+                a *= i;
+            } else {
+                b *= i;
+            }
+        }
+        return (b / a) / (N + 1);
+    }
 }
