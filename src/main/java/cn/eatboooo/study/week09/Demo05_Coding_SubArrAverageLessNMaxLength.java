fcda9bf1aadd090ab53372dc2fe7ec3808bfacee
禁止种太阳 <571241210@qq.com>
2022-02-13 17:49:21 +0800

    coding 无序数组arr 借助 minSum 和 minSumEnd 求子数组平均值小于等于v的最长子数组长度 智商压制

--- src/main/java/cn/eatboooo/study/week09/Demo05_Coding_SubArrAverageLessNMaxLength.java
+++ src/main/java/cn/eatboooo/study/week09/Demo05_Coding_SubArrAverageLessNMaxLength.java
@@ -6,6 +6,11 @@ package cn.eatboooo.study.week09;
  * @date 2022/2/12 22:03
  *
  * 给定一个数组arr，给定一个值v，求子数组平均值小于等于v的最长子数组长度
+ *
+ * 假设 v = 10
+ * 求的答案不就是 数组所有位置减 10，累加和 《= 0 的
+ *
+ * 所以直接参考 Demo05_Coding_SubArrLessNMaxLength
  */
 public class Demo05_Coding_SubArrAverageLessNMaxLength {
 }
