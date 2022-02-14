9d187ebc0a9f29d63a07eefc802fcd240baecf6a
禁止种太阳 <571241210@qq.com>
2022-02-14 21:51:50 +0800

    四边形 改正了，画家问题

--- src/main/java/cn/eatboooo/study/week09/Demo06_Try_Painter.java
+++ src/main/java/cn/eatboooo/study/week09/Demo06_Try_Painter.java
@@ -21,7 +21,6 @@ package cn.eatboooo.study.week09;
  * 返回4
  */
 public class Demo06_Try_Painter {
-    // bad
     public static int splitArray1(int[] arr, int num) {
         int[] sum = new int[arr.length + 1];
         for (int i = 0; i < sum.length - 1; i++) {
@@ -32,13 +31,12 @@ public class Demo06_Try_Painter {
 
     // 0~index 分成 num 份，返回所有份中的最大值
     private static int dp1(int[] arr, int index, int num) {
+        if (index == 0) {
+            return arr[1];
+        }
         if (num == 1) {
             return mysum(arr, 0, index);
         }
-        if (index == 0) {
-            return arr[0];
-        }
-
         int ans = Integer.MAX_VALUE;
         for (int i = index; i >= 0; i--) {
             // i 代表我要负责 i ~ index 位置的画
@@ -48,15 +46,12 @@ public class Demo06_Try_Painter {
             int cur = Math.max(l, r);
             if (cur < ans) {
                 ans = cur;
-            }
-        }
+            }		}
         return ans;
     }
 
     public static int mysum(int[] arr, int l, int r) {
         return arr[r + 1] - arr[l];
     }
-
-    //
 
 }
