45c7b3370ec363524c1725969864eb04d28729fc
禁止种太阳 <571241210@qq.com>
2022-02-14 18:25:04 +0800

    todo

---
+++ src/main/java/cn/eatboooo/study/week09/Demo06_Try_CrazyStone.java
@@ -0,0 +1,19 @@
+package cn.eatboooo.study.week09;
+
+/**
+ * 四边形
+ *
+ * @author weiZhiLin
+ * @version 1.0
+ * @date 2022/2/14 17:22
+ * <p>
+ * 摆放着n堆石子。现要将石子有次序地合并成一堆，规定每次只能选相邻的2堆石子合并成新的一堆
+ * 并将新的一堆石子数记为该次合并的得分，求出将n堆石子合并成一堆的最小得分（或最大得分）合并方案
+ */
+public class Demo06_Try_CrazyStone {
+    // arr【i】 代表 合并 i 石子要付出的代价，合并后的石头变大，代价变大
+    // 返回 在只能相邻两两合并的前提下的最小代价
+    public static int min1(int[] arr) {
+
+    }
+}
