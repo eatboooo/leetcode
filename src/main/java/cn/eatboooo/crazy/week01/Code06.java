b94e1455619afa4acbb9563704f0953dec0ae657
禁止种太阳 <571241210@qq.com>
2022-03-28 16:09:31 +0800

    week01，打怪兽。 todo，线段树版本

--- src/main/java/cn/eatboooo/crazy/week01/Code06.java
+++ src/main/java/cn/eatboooo/crazy/week01/Code06.java
@@ -14,10 +14,40 @@ package cn.eatboooo.crazy.week01;
  * 思路：线段树
  * 贪心：从最左正数范围开始，放完 AOE 再找最左正数...
  */
+// ⚠️ range 指的是，选定一只怪兽，距离他左边 range 和 右边 range 的都会收到伤害
+// TODO 线段树版本
 public class Code06 {
     // 贪心策略：永远让最左边缘以最优的方式(AOE尽可能往右扩，最让最左边缘盖住目前怪的最左)变成0，也就是选择：
     // 一定能覆盖到最左边缘, 但是尽量靠右的中心点
     // 等到最左边缘变成0之后，再去找下一个最左边缘...
     public static int minAoe2(int[] x, int[] hp, int range) {
+        int ans = 0;
+        int N = x.length;
+        for (int i = 0; i < N; i++) {
+            if (hp[i] > 0) {
+                // 找到释放 aoe 的中心位置
+                int mid = i;
+                while (mid < N && x[mid] - x[i] <= range) {
+                    mid++;
+                }
+                ans += hp[i];
+                // 释放技能
+                aoe(x, hp, i, mid - 1, range);
+            }
+        }
+        return ans;
+    }
+
+    private static void aoe(int[] x, int[] hp, int left, int mid, int range) {
+        int N = x.length;
+        int right = mid;
+        while (right < N && x[right] - x[mid] <= range) {
+            right++;
+        }
+        // ⚠️ 必须提取出来，因为 hp left 会发生改变
+        int i1 = hp[left];
+        for (int i = left; i < right; i++) {
+            hp[i] = Math.max(0, hp[i] - i1);
+        }
     }
 }
